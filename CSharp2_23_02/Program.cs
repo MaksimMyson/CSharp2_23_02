c86612fab781f27fcba7c63cda52f8b9f83fc72a
MaksimMyson <maksimmyson33@gmail.com>
2024-02-26 22:42:10 +0200

    Add project files.

---
+++ CSharp2_23_02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp2_23_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, C#!");
+        }
+    }
+}
