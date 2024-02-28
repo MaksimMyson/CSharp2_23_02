24a7160e05750f1970d94642a3d5c6a2d7939ab2
MaksimMyson <maksimmyson33@gmail.com>
2024-02-28 16:41:59 +0200

    exe6

--- CSharp2_23_02/Program.cs
+++ CSharp2_23_02/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace CSharp2_23_02
+﻿using System.Text.RegularExpressions;
+namespace CSharp2_23_02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, C#!");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Введіть текст:");
+            string inputText = Console.ReadLine();
+
+            string result = ChangeCaseOfSentences(inputText);
+
+            Console.WriteLine("Результат:");
+            Console.WriteLine(result);
+
+            Console.ReadLine(); 
+        }
+
+        static string ChangeCaseOfSentences(string text)
+        {
+            
+            string[] sentences = Regex.Split(text, @"(?<=[\.!\?])\s+");
+
+            
+            for (int i = 0; i < sentences.Length; i++)
+            {
+                if (!string.IsNullOrWhiteSpace(sentences[i]))
+                {
+                    sentences[i] = char.ToUpper(sentences[i][0]) + sentences[i].Substring(1);
+                }
+            }
+
+            
+            return string.Join(" ", sentences);
         }
     }
 }
